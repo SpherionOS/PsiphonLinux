5402c8f8ecf0dfc8cf7573a814d1d7e1e712424e
SpherionOS <122868668+SpherionOS@users.noreply.github.com>
2023-11-04 15:47:22 +1000

    Update plinstaller.sh

--- plinstaller.sh
+++ plinstaller.sh
@@ -9,5 +9,20 @@ wget https://raw.githubusercontent.com/SpherionOS/PsiphonLinux/main/psiphon.sh
 chmod +x ./psiphon-tunnel-core-x86_64
 chmod +x ./psiphon.sh 
 
+echo " "
+echo "Running post install checks"
+
+if test -f psiphon-tunnel-core-x86_64; then
+  echo "Psiphon binary found"
+fi
+
+if test -f psiphon.config; then
+  echo "Psiphon configuration found"
+fi
+
+if test -f psiphon.sh; then
+  echo "Psiphon start script found"
+fi
+
 #Finish installer and give message
 echo "Psiphon Linux installation successfully completed. Run ./psiphon.sh with root permissions to run the program. Please allow a reasonable amount of time to elapse for Psiphon to connect"
